73081e93d358438e2c748dae0959f379b74315f7
Afonso Machado <afonso777@hotmail.com>
2020-10-12 16:10:11 -0300

    inserindo mapa natela

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../images/map-marker.svg';
 
@@ -22,8 +25,18 @@ function OrphanagesMap() {
           <span>Bahia</span>
         </footer>
       </aside>
-
-      <div></div>
+      {/* parametros de exeibição do mapa */}
+      <Map
+        center={[12.2587348, -38.9598163]}
+        zoom={14}
+        style={{
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        {/* escolhendo open street map para exibição */}
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      </Map>
 
       <Link to="" className="create-orphanage">
         <FiPlus size={32} color="#fff" />
