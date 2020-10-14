b9736a5bf26c6eea19a8f3b5f536da9d5666735f
Afonso Machado <afonso777@hotmail.com>
2020-10-14 12:22:22 -0300

    criando migration para o db

---
+++ happy-server/src/database/migrations/1602687809464-create_orphanages.ts
@@ -0,0 +1,58 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class createOrphanages1602687809464 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // Realizar alterações no DB
+    await queryRunner.createTable(
+      new Table({
+        name: 'orphanages',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            unsigned: true,
+            isPrimary: true,
+            // gerada automaticamente
+            isGenerated: true,
+            // auto increment
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'latitude',
+            type: 'decimal',
+            scale: 10,
+            precision: 2,
+          },
+          {
+            name: 'longitude',
+            type: 'decimal',
+            scale: 10,
+            precision: 2,
+          },
+          {
+            name: 'about',
+            type: 'text',
+          },
+          {
+            name: 'instructions',
+            type: 'text',
+          },
+          {
+            name: 'open_on_weekend',
+            type: 'boolean',
+            default: false,
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    // Desfazer o que foi feitno no up
+    await queryRunner.dropTable('orphanages');
+  }
+}
--- happy-server/src/server.ts
+++ happy-server/src/server.ts
@@ -1,5 +1,6 @@
 import express, { response } from 'express';
 
+// connectando com o db
 import './database/connection';
 
 // incializando app express, para lidar com rotas da aplicação
@@ -15,43 +16,5 @@ app.get('/users', (request, response) => {
 
 app.listen(port);
 console.log(`Servidor executando na porta ${port}`);
+
 // backend executando em localhost:3333
-
-/**
- * Rota = conjunto
- * Recurso = usuario
- *
- * Métodos HTTP: GET, POST, PUT, DELETE
- *
- * GET: Buscar uma informação (lista, itens)
- * POST: Criando uma informação nova
- * PUT: editando uma informação
- * DELETE: deletando uma informação
- *
- * Parâmetros:
- *
- * Query Params: http://localhost:3333/users?search=fulano
- * Route Params: http://localhost:3333/users/1 (identificar um recurso)
- * Body: http://localhost:3333/users (Corpo da requisição)
- *
- */
-
-// app.get('/users/:id', (req, res) => {
-//   // query paramns
-//   console.log(req.query);
-//   // route params
-//   console.log(req.params);
-//   // body
-//   console.log(req.body);
-
-//   return res.json({ message: 'hello' });
-// });
-
-/**
- * Formas de acessar banco de dados
- *
- * Driver nativo: SQL
- * Query Builder: Knex
- * ORM (Objecr Relation Mapping): cada elemendo do db é uma instancia de uma classe referente a ele no app
- *
- */
