2d9a5e84dd76b8aec8e0e305472b4a65b1e717bb
Afonso Machado <afonso777@hotmail.com>
2020-10-14 13:58:17 -0300

    upload de imagens ok!

---
+++ happy-server/src/config/upload.ts
@@ -0,0 +1,17 @@
+import multer from 'multer';
+import path from 'path';
+
+// multer lida com armazenamento de imagens
+
+export default {
+  storage: multer.diskStorage({
+    // __dirname é o diretorio atual
+    destination: path.join(__dirname, '..', '..', 'uploads'),
+    filename: (request, file, cb) => {
+      // dar um nome para o arquivo
+      const fileName = `${Date.now()}-${file.originalname}`;
+      // erro, nome do arquivo
+      cb(null, fileName);
+    },
+  }),
+};
