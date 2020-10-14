dfa091e79d0afadc792532a361be985315ae497b
Afonso Machado <afonso777@hotmail.com>
2020-10-14 12:52:41 -0300

    cadastro de orfanato funcionando

--- happy-server/src/server.ts
+++ happy-server/src/server.ts
@@ -1,4 +1,6 @@
 import express, { response } from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from './models/Orphanage';
 
 // connectando com o db
 import './database/connection';
@@ -10,7 +12,34 @@ const app = express();
 // habilitando uso do json
 app.use(express.json());
 
-app.post('/orphanages', (request, response) => {
+app.post('/orphanages', async (request, response) => {
+  // capturando dados enviados
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekend,
+  } = request.body;
+
+  const orphanagesRepository = getRepository(Orphanage);
+
+  // criando um novo orfanato, deixando pré criado
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekend,
+  });
+
+  // salvando no db
+  await orphanagesRepository.save(orphanage);
+
   return response.json({ message: 'hello' });
 });
 
