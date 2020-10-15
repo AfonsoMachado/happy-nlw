e4db15a663de0d95668e6d1e95beefdb78c20577
Afonso Machado <afonso777@hotmail.com>
2020-10-15 12:01:46 -0300

    novas paginas

--- happy-web/src/routes.tsx
+++ happy-web/src/routes.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from './pages/Landing';
 import OrphanagesMap from './pages/OrphanagesMap';
+import Orphanage from './pages/Orphanage';
+import CreateOrphanage from './pages/CreateOrphanage';
 
 function Routes() {
   return (
@@ -12,6 +14,8 @@ function Routes() {
         {/* exact para mostrar somente a rota indicada, excluindo as outras */}
         <Route path="/" exact component={Landing} />
         <Route path="/app" component={OrphanagesMap} />
+        <Route path="/orphanages/create" component={CreateOrphanage} />
+        <Route path="/orphanages/:id" component={Orphanage} />
       </Switch>
     </BrowserRouter>
   );
