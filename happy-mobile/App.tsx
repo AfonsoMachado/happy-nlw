ef12e937bf2e7d4e882ec2de19b3f0f2350719f5
Afonso Machado <afonso777@hotmail.com>
2020-10-17 15:04:29 -0300

    marcador do mapa

--- happy-mobile/App.tsx
+++ happy-mobile/App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps';
 
 import mapMarker from './src/images/map-marker.png';
 
@@ -20,11 +20,21 @@ export default function App() {
       >
         <Marker
           icon={mapMarker}
+          calloutAnchor={{
+            x: 2.7,
+            y: 0.8,
+          }}
           coordinate={{
             latitude: -12.2587348,
             longitude: -38.9598163,
           }}
-        />
+        >
+          <Callout tooltip onPress={() => {}}>
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>Lar das meninas</Text>
+            </View>
+          </Callout>
+        </Marker>
       </MapView>
     </View>
   );
@@ -38,5 +48,19 @@ const styles = StyleSheet.create({
   map: {
     width: Dimensions.get('screen').width,
     height: Dimensions.get('screen').height,
+  },
+
+  calloutContainer: {
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255,255,255,0.8)',
+    borderRadius: 16,
+    justifyContent: 'center',
+  },
+
+  calloutText: {
+    color: '#0089a5',
+    fontSize: 14,
   },
 });
