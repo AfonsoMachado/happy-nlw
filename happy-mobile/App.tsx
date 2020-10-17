b9d5c442fb5a0c985b36d46cbb65688e79d920d5
Afonso Machado <afonso777@hotmail.com>
2020-10-17 14:53:33 -0300

    adicionando marcador no mapa

--- happy-mobile/App.tsx
+++ happy-mobile/App.tsx
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+
+import mapMarker from './src/images/map-marker.png';
 
 export default function App() {
   return (
@@ -15,7 +17,15 @@ export default function App() {
           latitudeDelta: 0.008,
           longitudeDelta: 0.008,
         }}
-      />
+      >
+        <Marker
+          icon={mapMarker}
+          coordinate={{
+            latitude: -12.2587348,
+            longitude: -38.9598163,
+          }}
+        />
+      </MapView>
     </View>
   );
 }
@@ -26,7 +36,7 @@ const styles = StyleSheet.create({
   },
 
   map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    width: Dimensions.get('screen').width,
+    height: Dimensions.get('screen').height,
   },
 });
